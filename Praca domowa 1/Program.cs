4a112e70c47d13319e7b19823f4daefd88a03f06
Ania <anami@onet.eu>
2023-02-05 13:54:50 +0100

    Praca domowa 1

---
+++ Praca domowa 1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string myName = "Anna";
+
+int myAge = 25;
+
+string mySex = "female";
+
+string mySex2 = "men";
+
+
+if (myName == "Ania" && myAge > 30);
+{
+    Console.WriteLine("Ania Lat 25");
+}
+
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+string result2 = mySex2;
+    if (mySex2 == "men && age2 >18");
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
